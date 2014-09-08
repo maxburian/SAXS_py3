b891bcb409d424d92473f983a31820da19570f14
Christian Meisenbichler <christian@mur.at>
2014-09-08 09:36:01 +0200

    debug network

--- SAXS/NetConf.py
+++ SAXS/NetConf.py
@@ -1,21 +1,39 @@
 import random
 import json
 import os,sys
+from optparse import OptionParser
+from subprocess import call
 from jsonschema import validate,ValidationError
 def createsaxdogconf():
-    server=raw_input( "Please enter the Saxsdog Server URL (tcp://hostname:port):\n")
-    feeder=raw_input("Please enter the file events feeder service URL (tcp://hostname:port):\n")
-    secret='%030x' % random.randrange(16**30)
-    if server=="":server="tcp://localhost:9723"
-    if feeder=="":feeder="tcp://localhost:9823"
-    content={"Server":server,"Feeder":feeder,"Secret":secret}
+    parser = OptionParser()
+    usage = "usage: %prog [options] "
+    parser = OptionParser(usage)
+    parser.add_option("-n", "--new", dest="new",  action="store_true",default=False,
+                      help="replace old config and generate new secret")
+    (options, args) = parser.parse_args(args=None, values=None)
+    
+    confpath=os.path.expanduser("~"+os.sep+".saxsdognetwork")
+    if os.path.isfile(confpath) and not options.new:
+        content=json.load(open(confpath))
+    else:
+        server="tcp://localhost:9723"
+        feeder="tcp://localhost:9823"
+        secret='%030x' % random.randrange(16**30)
+        content={"Server":server,"Feeder":feeder,"Secret":secret}
+        json.dump(content,open(confpath,"w"),indent=4, separators=(',', ': '))
+    
+    if  os.name == "nt":
+        call(["notepad",confpath ])
+    elif os.name == "posix": 
+        call(["vi",confpath ])
+    content=json.load(open(confpath))
     try:
         validate(content,json.load(open(os.path.dirname(__file__)+os.sep+'NetworkSchema.json')))
     except ValidationError as e:
         print "Error in config file: ",e.message
         sys.exit()
-    json.dump(content,open(os.path.expanduser("~"+os.sep+".saxdognetwork"),"w"))
-    print os.path.expanduser("~"+os.sep+".saxdognetwork")," created. \n\n Copy it to the other machines who take part in your network"
+    json.dump(content,open(confpath,"w"))
+    print confpath," created. \n\n Copy it to the other machines who take part in your network"
 
 if __name__ == '__main__':
     createsaxdogconf()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ try:
 except:
     print "No py2exe here"
  
-
+from subprocess import call
  
  
 
@@ -45,4 +45,51 @@ setup(
         
     }
 )
- 
+
+if sys.argv[1] == 'install':
+    
+    
+    if os.name == "nt":
+        import _winreg as  wr
+      
+        pyw_executable =   os.path.join(sys.prefix,'pythonw.exe')
+        script_file =  os.path.join(sys.prefix,"Scripts","Leash-script.pyw")
+        iconpath= os.path.expanduser(
+            os.path.join(
+            sys.prefix,
+            "Lib",
+            "site-packages", 
+            "SAXS",
+            "icons",
+            "program.ico"))
+        wr.SetValue(wr.HKEY_CURRENT_USER,"Software\Classes\TUG.ortlight\shell\open\command",wr.REG_SZ,pyw_executable+" \""+ script_file+ "\"  \"%1\"")
+      
+        wr.SetValue( wr.HKEY_CURRENT_USER,"Software\Classes\TUG.saxsdog\DefaultIcon",wr.REG_SZ,iconpath)
+        wr.SetValue( wr.HKEY_CURRENT_USER,"Software\Classes\.ortl",wr.REG_SZ, "TUG.SAXSdog");
+        
+        print "added registry values for extensions"
+         
+        import winshell
+        import win32com
+         
+        pyw_executable =   os.path.join(sys.prefix,'pythonw.exe')
+        script_file =  os.path.join(sys.prefix,"Scripts","Leash-script.pyw")
+        w_dir = os.path.expanduser('~')
+        desktop_path = winshell.desktop()
+        startmenu_path =win32com.shell.shell.SHGetSpecialFolderPath(0, win32com.shell.shellcon.CSIDL_STARTMENU)
+        with winshell.shortcut( os.path.join(startmenu_path,'SAXSdog.lnk')) as link:
+            link.path= pyw_executable
+            link.description =  "Control panel for SAXSdog Server"
+            link.arguments =  script_file
+            link.icon_location=(iconpath,0)
+        with winshell.shortcut( os.path.join(desktop_path,'SAXSdog.lnk')) as link:
+            link.path= pyw_executable
+            link.description = "Control panel for SAXSdog Server"
+            link.arguments =  script_file
+            link.icon_location=(iconpath,0)
+        print startmenu_path
+        call(["ie4uinit.exe" ,"-ClearIconCache"])
+    elif os.name == "posix":
+        print os.name
+       
+        call(["bash","addmime.sh"])
