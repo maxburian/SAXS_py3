c380f0b79c76fef8cea1a1bcc2518ab70c232412
Christian Meisenbichler <christian@mur.at>
2014-09-18 13:55:46 +0200

    missing comma

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
                       "jsonschema", 
                       "bitarray",
                       "watchdog",
-                      "python-daemon"
+                      "python-daemon",
                       "sphinxcontrib-programoutput",
                       "sphinxcontrib-programscreenshot",
                       "pyzmq"],
