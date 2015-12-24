5397d2a172e984b2adc45b116933be1e0929a716
Max Burian <burian@tugraz.at>
2015-12-24 10:03:41 +0100

    Added a security feature to check for user folder before loading the queue. Some adjustments of default values of json schema.

--- SAXS/leash2.py
+++ SAXS/leash2.py
@@ -17,6 +17,7 @@ import collections
 import consolidatepanel
 from multiprocessing import Process,Value
 from Server import saxsdogserver
+from PyQt4.Qt import QMessageBox
 class LeashUI(QtGui.QMainWindow):
     def __init__(self,app,parent=None):
         super(LeashUI,self).__init__(parent)
@@ -131,20 +132,26 @@ class LeashUI(QtGui.QMainWindow):
         self.calibeditor.model.save()
         argu=["new", data]
         result={}
-        try:
-            result=json.loads(Leash.initcommand(self.options,argu,self.netconf))
-        except Exception as e:
-            self.errormessage.setWindowTitle("Server Error")
-            self.errormessage.setMinimumSize(400, 300)
-            self.errormessage.showMessage( str(e))
-        if 'result' in result and result['result']=="new queue":
-            print result
-            self.setqueuesynced()
-            self.plotthread.start()
-        else:
-            self.errormessage.setWindowTitle("Server Error")
-            self.errormessage.setMinimumSize(400, 300)
-            self.errormessage.showMessage( result["data"]["Error"])
+        title = str(data['Title'])
+        usrfolder = str(data['Directory'][0])
+        titlestr='The title and the selected user folder do NOT match. Are you sure you are user: ' + str(usrfolder) +' ?' 
+        if title.find(usrfolder)==-1:
+            res=QMessageBox.warning(self, self.tr("User folder"),self.tr(titlestr), QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if res==QMessageBox.Yes :
+            try:
+                result=json.loads(Leash.initcommand(self.options,argu,self.netconf))
+            except Exception as e:
+                self.errormessage.setWindowTitle("Server Error")
+                self.errormessage.setMinimumSize(400, 300)
+                self.errormessage.showMessage( str(e))
+            if 'result' in result and result['result']=="new queue":
+                print result
+                self.setqueuesynced()
+                self.plotthread.start()
+            else:
+                self.errormessage.setWindowTitle("Server Error")
+                self.errormessage.setMinimumSize(400, 300)
+                self.errormessage.showMessage( result["data"]["Error"])
     def setqueuesynced(self):
         self.queuestatuslabel.setText("Queue started.")
         self.filestatuslabel.setText("Local calibration synced")
