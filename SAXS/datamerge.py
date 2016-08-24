cf11cf23e540194825c2cfdad5b59d1a0bfa6a9f
Max Burian <chmberg@gmail.com>
2016-08-24 15:32:01 +0200

    fixed typo bug

--- SAXS/datamerge.py
+++ SAXS/datamerge.py
@@ -505,7 +505,7 @@ def mergelogs(app,conf,attachment=None,directory="."):
                     zeroCorr=tmplog.index.min()
                     print zeroCorr
                     mergestatus="\nZero image corresponds to peakInteg time: " + pd.to_datetime(zeroCorr).strftime("%a, %d %b %Y %H:%M:%S")
-                    app.writeToMergestatus(mergestatus)
+                    app.writeToMergeStatus(mergestatus)
                     
             else:
                 logframe=logframe.append(tmplog).sort_index()
